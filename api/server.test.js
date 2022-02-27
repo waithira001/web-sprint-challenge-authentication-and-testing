d351464b6c33a2517c7d8a7120de54664f834063
Ruth Muiruri <rmuiruri911@gmail.com>
2022-02-26 20:05:00 -0600

    Update server.test.js

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,49 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const db = require('../data/dbConfig')
+const request = require('supertest')
+const server = require('./server')
+const auth = require('./auth/auth-router')
+const mike ={
+  username: 'mike',
+  password: 'cheese'
+}
+
+beforeAll( async ()=> {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+beforeEach(async ()=> {
+  await db('users').truncate()
+})
+
+afterAll( async ()=> {
+  await db.destroy()
+})
+
+describe('endpoints', () => {
+  describe(' [POST] /register', () => {
+    it('responds with a 201 ok', async ()=> {
+      const res = await request(auth).post('/register')
+    expect(res.status).toBe(201)
+    })
+    it('returns the right num of users', async ()=> {
+      let res
+      await db('users').insert(mike)
+      res = await request(auth).post('/register')
+      expect(res.body).toHaveLength(1)
+    })
+  })
+})
+
+describe('[POST] /login', () => {
+  it('responds with new created user', async () => {
+    let res 
+    res = await (await request(auth).post('/login')).send(mike)
+    expect(res.body).toMatchObject({id:1, ...mike})
+  })
+  it('responds with 200 ok', async () => {
+    const res = await request(auth).post('/login')
+    expect(res.status).toBe(200)
+  })
 })
