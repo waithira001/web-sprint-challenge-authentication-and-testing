c2f539d64c6574762dd43d7e778284d97f5eb432
Ruth Muiruri <rmuiruri911@gmail.com>
2022-02-26 20:04:51 -0600

    Create middleware.js

---
+++ api/middleware/middleware.js
@@ -0,0 +1,52 @@
+const Jokes = require('../jokes/jokes-data')
+
+
+const checkPayload = (req, res, next) => {
+    if(!req.body.username || !req.body.password ){
+        res.status(401).json({message: "username and password required"})
+    }
+    else{
+        next()
+    }
+}
+
+const checkUserInDB =  async (req,res,next) => {
+    try{
+        const row = await Jokes.findBy({username: req.body.username})
+        if(!row.length){
+            next()
+        }
+        else {
+            res.status(401).json("username taken")
+    } 
+    } catch(e) {
+        res.status(500).json(`server error: ${e.message}`)
+
+    }
+}
+
+
+
+const checkUsernameExists = async ( req, res, next) => {
+    try{
+      const rows = await Jokes.findByUserName({username: req.body.usernme})
+      if(rows.length.trim()){
+        req.userData = rows[0]
+        next()
+      }else{
+        res.status(401).json({
+          "message": "Invalid credentials"
+  
+        })
+      }
+      }catch(e){
+        res.status(500).json('Server broke')
+    }
+}
+
+module.exports = {
+    checkUsernameExists,
+    checkPayload,
+    checkUserInDB
+
+}
